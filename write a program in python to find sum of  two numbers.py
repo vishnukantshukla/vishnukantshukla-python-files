0cf892c3446631d8335812388986eef6ca53154a
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2022-02-06 18:57:33 +0530

    Add files via upload

---
+++ write a program in python to find sum of  two numbers.py
@@ -0,0 +1,4 @@
+first_number= input("please enter the first number ?")
+second_number=input("please enter the second number? ")
+sum = int(first_number)+int(second_number)
+print("The sum of two numbers :" + str(sum))
